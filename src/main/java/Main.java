23d17c024aae2533e49cee70bcc1e9f57f285499
Mahesh0101 <maheshababuv8321gmail.com>
2026-05-02 14:57:57 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,18 @@ public class Main {
             if (input.equals("exit")) {
                 break;
             }
+
+            // clean the input
+            input = input.trim();
+
+            // echo the input back to the users
+
+            if (input.startsWith("echo")) {
+                String echoOutput = input.substring(4).trim();
+                System.out.println(echoOutput);
+                continue;
+            }
+
             System.out.println(input + ": command not found");
         }
     }
