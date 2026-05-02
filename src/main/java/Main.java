3e3a80c046cb7cff321b4b23a0cacf1a83cd9e41
Mahesh0101 <maheshababuv8321gmail.com>
2026-05-02 15:34:34 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,42 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
+        HashSet<String> commands = new HashSet<>();
+        commands.add("echo");
+        commands.add("type");
+        commands.add("exit");
+
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
 
-            if (input.equals("exit")) {
-                break;
-            }
-
             // clean the input
             input = input.trim();
 
+            if (input.equals("exit")) {
+                break;
+            }
             // echo the input back to the users
-
-            if (input.startsWith("echo")) {
+            else if (input.startsWith("echo")) {
                 String echoOutput = input.substring(4).trim();
                 System.out.println(echoOutput);
-                continue;
-            }
 
-            System.out.println(input + ": command not found");
+            } else if (input.startsWith("type")) {
+                String cmd = input.substring(4).trim();
+
+                if (commands.contains(cmd)) {
+                    System.out.println(cmd + " is a shell builtin");
+                } else {
+                    System.out.println(cmd + ": not found");
+                }
+
+            } else {
+                System.out.println(input + ": command not found");
+            }
         }
     }
 }
