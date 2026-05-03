89c12e79b32bddd4028cc4fd201039ccb6e361e3
Mahesh0101 <maheshababuv8321gmail.com>
2026-05-03 23:02:06 +0530

    codecrafters submit [skip ci]

--- src/main/java/CommandRegistry.java
+++ src/main/java/CommandRegistry.java
@@ -1,3 +1,4 @@
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +18,7 @@ public class CommandRegistry {
         builtins.put("echo", command -> System.out.println(command.rawArgs()));
         builtins.put("pwd", command -> System.out.println(System.getProperty("user.dir")));
         builtins.put("type", command -> handleType(command));
+        builtins.put("cd", command -> handleChangeDirectory(command));
         builtins.put("exit", command -> System.out.println("Exiting...")); // does not actually exit, just a placeholder
                                                                            // lambda. we are checking for this in the
                                                                            // main loop and will break out of it when we
@@ -52,5 +54,24 @@ public class CommandRegistry {
 
     public Set<String> builtinNames() {
         return builtins.keySet();
+    }
+
+    public void handleChangeDirectory(Command command) {
+
+        String path = command.args().get(0);
+        Path currentDir = Path.of(System.getProperty("user.dir"));
+        Path newPath = currentDir.resolve(path).normalize();
+
+        if (!Files.exists(newPath)) {
+            System.out.println("cd: " + path + ": No such file or directory");
+        } else if (!Files.isDirectory(newPath)) {
+            System.out.println("cd: " + path + ": Not a directory");
+        } else {
+            try {
+                System.setProperty("user.dir", newPath.toString());
+            } catch (Exception e) {
+                System.out.println("cd: " + e.getMessage());
+            }
+        }
     }
 }
